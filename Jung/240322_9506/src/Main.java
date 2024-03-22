3fb595adf4b316dae34aa596db7a8ebd18d393c1
study2895 <144806675+study2895@users.noreply.github.com>
2024-03-22 22:42:24 +0900

    delete if statement

--- Jung/240322_9506/src/Main.java
+++ Jung/240322_9506/src/Main.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);	
+		Scanner sc = new Scanner(System.in);
+		
 		while(true) {
 			int n = sc.nextInt();
 			if(n==-1) break;
@@ -15,17 +16,16 @@ public class Main {
 public static void findPerfect(int n) {
 	int m = 0; int k = 0;
 	int[] arr = new int[n];
-	
 	for(int i=1; i<=n; i++) {
 		if(n%i==0) {arr[m++]=i; k+=i;}
 	} k-=n;
 	if(k==n) {
 		System.out.print(n+" = ");
 		for(int i=0; i<m-1; i++) {
-			if(i==m-2) System.out.print(arr[i]+"\n");
-			else System.out.print(arr[i]+" + ");
+			System.out.print(arr[i]);
+			if(i!=m-2) System.out.print(" + ");
 		}
 	}
-	else System.out.print(n+" is NOT perfect.\n");
+	else System.out.print("\n"+n+" is NOT perfect.\n");
 	}
 }
